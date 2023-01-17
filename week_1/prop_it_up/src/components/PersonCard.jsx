7e4d0300c0ce4c916dbde34084d8e48ce8cf9241
Rony Pablo <ronyalexis0@gmail.com>
2023-01-17 12:32:13 -0800

    added useState Hook - increase age by 1

--- week_1/prop_it_up/src/components/PersonCard.jsx
+++ week_1/prop_it_up/src/components/PersonCard.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard =(props) => {
+    const [age, setAge] = useState(props.age);
+
+    const birthday = () => {
+        setAge(age + 1 )
+    }
+
     return (
         <div>
             <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={birthday}>Set</button>
         </div>
     )
 }
