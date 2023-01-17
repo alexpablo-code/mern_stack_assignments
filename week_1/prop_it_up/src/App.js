6606fe5c95bc4f15978522551f94ab24eeea848b
Rony Pablo <ronyalexis0@gmail.com>
2023-01-16 16:43:18 -0800

    prop it up assignment

---
+++ week_1/prop_it_up/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard
+        firstName= {'Thor'}
+        lastName = {'Odinson'}
+        age = {1500}
+        hairColor = {'Blonde'}
+      />
+      <PersonCard
+        firstName= {'Loki'}
+        lastName = {'Odinson'}
+        age = {1054}
+        hairColor = {'Black'}
+      />
+      <PersonCard
+        firstName= {'Wanda'}
+        lastName = {'Maximoff'}
+        age = {29}
+        hairColor = {'Brown'}
+      />
+      <PersonCard
+        firstName= {'Clint'}
+        lastName = {'Barton'}
+        age = {53}
+        hairColor = {'Dark Brown'}
+      />
+    </div>
+  );
+}
+
+export default App;
