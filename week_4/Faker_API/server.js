6ec1604b8decc691409c1bb6bf446913faf7ea0e
Rony Pablo <ronyalexis0@gmail.com>
2023-02-02 10:14:35 -0800

    adding routes

--- week_4/Faker_API/server.js
+++ week_4/Faker_API/server.js
@@ -21,9 +21,31 @@ const users = [
 
 const createUser = () => {
     const newUser = {
-        password: faker.name.firstName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.number(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        id: faker.database.mongodbObjectId()
+
     };
     return newUser;
+}
+
+const createCompany = () => {
+    const newCompany = {
+        id: faker.database.mongodbObjectId(),
+        name: faker.company.name(),
+        address: {
+            street: faker.address.street(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+
+    };
+    return newCompany;
 }
 
 
@@ -41,6 +63,13 @@ app.get("/api/users/new", (req, res) => {
     res.json(createUser());
 });
 
+app.get("/api/companies/new", (req, res) => {
+    res.json(createCompany());
+});
+
+app.get("/api/user/company", (req, res) => {
+    res.json([createCompany(), createUser()]);
+});
 
 
 // this needs to be below the other code blocks
