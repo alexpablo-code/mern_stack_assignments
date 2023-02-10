e57104b7f748fdfbe11aaeec91f888fcafc8db5f
Rony Pablo <ronyalexis0@gmail.com>
2023-02-09 22:49:54 -0800

    adding validations to Schema

--- week_5/mongoose_practice/server/models/movie.js
+++ week_5/mongoose_practice/server/models/movie.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
 const MovieSchema = new mongoose.Schema({
-    //this method takes in a object as a parameter, THE STRUCTURE OF THIS OBJECT IS HOW EACH NEW DOCUMENT PUT INTO COLLECTION WILL BE FORMATED
-    movieName: String,
+    //this meth{{od takes in a object as a parameter, THE STRUCTURE OF THIS OBJECT IS HOW EACH NEW DOCUMENT PUT INTO COLLECTION WILL BE FORMATED
+    movieName: {
+        type: String,
+        required:[true, "Name of Movie required"],
+        minlength:[3, "Movie name must be at least 3 characters long"]
+    },
     // we can also write it -. name: string,
-    movieYear: Number,
-    movieDescription: String
+    movieYear: {
+        type:Number,
+        required:[true, "Movie Release year is required"],
+        min:[1878, "Movies were not made yet before 1878"],
+        max:[2025, "Are you from the future, no spoilers please"]
+    },
+
+    movieDescription: {
+        type: String,
+        required:[true, "Movie deswcription required"],
+        minlength:[3, "Movie description must be at least 3 characters long"]
+    },
 
 }, {timestamps: true});
 // HERE we are adding a second argument, this will automatically add timestamp properies to our document like created_at and updated_at
