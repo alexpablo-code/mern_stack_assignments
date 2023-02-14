103f81c085e900f65a990398c82d7ad3ead77792
Rony Pablo <ronyalexis0@gmail.com>
2023-02-14 00:02:59 -0800

    first commit

---
+++ week_6/product_manager/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+require('./config/mongoose.config');
+
+app.use(express.json(), express.urlencoded({extended: true}));
+
+//we will add the require routes here later
+
+
+app.listen(8000, () => console.log("The server is all fired up on port 8000"));
