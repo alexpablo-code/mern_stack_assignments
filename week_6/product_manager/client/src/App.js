780429bc293ec91d831f67e89a5d932010aa431e
Rony Pablo <ronyalexis0@gmail.com>
2023-02-15 12:33:18 -0800

    Product form added and working

--- week_6/product_manager/client/src/App.js
+++ week_6/product_manager/client/src/App.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+import ProductForm from './components/ProductForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <ProductForm/>
     </div>
   );
 }
--- week_6/product_manager/client/src/components/ProductForm.js
+++ week_6/product_manager/client/src/components/ProductForm.js
@@ -1,21 +1,40 @@
 import {useState, useEffect} from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 const ProductForm = () => {
+    const[title, setTitle] = useState("");
+    const[price, setPrice] = useState();
+    const[description, setDescription] = useState("");
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:8000/api/addproduct', {
+            title, price, description
+        })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log("Something went wrong", err)
+            })
+    }
+
+
     return (
         <div>
-            <form onSubmit={}>
+            <form onSubmit={onSubmitHandler}>
                 <div>
                     <label htmlFor="">Title</label>
-                    <input type="text" />
+                    <input type="text" name='title' onChange={(e) => setTitle(e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor="">Price</label>
-                    <input type="number" />
+                    <input type="number" name='price' onChange={(e) => setPrice(e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor="">Description</label>
-                    <input type="text" />
+                    <textarea name="description" id="" cols="30" rows="10" onChange={(e)=> setDescription(e.target.value)}></textarea>
                 </div>
                 <button type="submit">Submit</button>
             </form>
