620159e2fa8b304d4fab45c686482cf5dea69893
Rony Pablo <ronyalexis0@gmail.com>
2023-02-19 10:41:47 -0800

    adding validations to edit form

--- week_6/product_manager/client/src/components/EditForm.js
+++ week_6/product_manager/client/src/components/EditForm.js
@@ -2,12 +2,25 @@ import {useState, useEffect} from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const EditForm = ({product}) => {
+const EditForm = ({id}) => {
     const navigate = useNavigate();
+    const[errors, setErrors] = useState({})
     
 
 
-    const [updatedProduct, setUpdatedProduct] = useState(product);
+    const [updatedProduct, setUpdatedProduct] = useState({});
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/allproducts/${id}`)
+            .then((oneProduct) => {
+                console.log(oneProduct.data)
+                setUpdatedProduct(oneProduct.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    },[])
 
 
 
@@ -18,13 +31,14 @@ const EditForm = ({product}) => {
     const onSubmitHandler = (e) => {
         e.preventDefault();
         console.log(updatedProduct)
-        axios.put(`http://localhost:8000/api/allproducts/${product._id}/edit`, updatedProduct)
+        axios.put(`http://localhost:8000/api/allproducts/${id}/edit`, updatedProduct)
             .then((result) => {
                 console.log(result);
                 navigate('/allproducts')
             })
             .catch((err) => {
                 console.log(err);
+                setErrors(err.response.data.errors)
             })
     }
 
@@ -32,19 +46,34 @@ const EditForm = ({product}) => {
         <section className='sectionContainer'>
             <div className='formContainer'>
                 <form className='productForm' onSubmit={onSubmitHandler}>
-                    <h4>{product.title}</h4>
+                    <h4>{updatedProduct.title}</h4>
                     <div className='inputGroup'>
                         <label htmlFor="">Title</label>
-                        <input type="text" name='title' onChange={handleInputChange} value={updatedProduct.title || product.title}/>
+                        <input type="text" name='title' onChange={handleInputChange} value={updatedProduct.title}/>
                     </div>
+                        {
+                            errors.title?
+                            <p className='errors'>{errors.title.message}</p>:
+                            null
+                        }
                     <div className='inputGroup'>
                         <label htmlFor="">Price</label>
-                        <input type="number" step="0.01" name='price' onChange={handleInputChange} value={updatedProduct.price || product.price}/>
+                        <input type="number" step="0.01" name='price' onChange={handleInputChange} value={updatedProduct.price}/>
                     </div>
+                        {
+                            errors.price?
+                            <p className='errors'>{errors.price.message}</p>:
+                            null
+                        }
                     <div className='inputGroup'>
                         <label htmlFor="">Description</label>
-                        <textarea name="description" id="" cols="30" rows="10" onChange={handleInputChange} value={updatedProduct.description || product.description}></textarea>
+                        <textarea name="description" id="" cols="30" rows="10" onChange={handleInputChange} value={updatedProduct.description}></textarea>
                     </div>
+                        {
+                            errors.description?
+                            <p className='errors'>{errors.description.message}</p>:
+                            null
+                        }
                     <div className='objCenter'>
                     <button type="submit">Submit</button>
                     </div>
--- week_6/product_manager/client/src/components/ProductForm.js
+++ week_6/product_manager/client/src/components/ProductForm.js
@@ -35,14 +35,29 @@ const ProductForm = () => {
                         <label htmlFor="">Title</label>
                         <input type="text" name='title' onChange={(e) => setTitle(e.target.value)}/>
                     </div>
+                        {
+                            errors.title?
+                            <p className='errors'>{errors.title.message}</p>:
+                            null
+                        }
                     <div className='inputGroup'>
                         <label htmlFor="">Price</label>
                         <input type="number" step="0.01" name='price' onChange={(e) => setPrice(e.target.value)}/>
                     </div>
+                        {
+                            errors.price?
+                            <p className='errors'>{errors.price.message}</p>:
+                            null
+                        }
                     <div className='inputGroup'>
                         <label htmlFor="">Description</label>
                         <textarea name="description" id="" cols="30" rows="10" onChange={(e)=> setDescription(e.target.value)}></textarea>
                     </div>
+                        {
+                            errors.description?
+                            <p className='errors'>{errors.description.message}</p>:
+                            null
+                        }
                     <div className='objCenter'>
                     <button type="submit">Submit</button>
                     </div>
